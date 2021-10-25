8773e596c22c9d466d3e66896c80adaaaa2dd1d5
Cristian Nicolae Lupascu <cristian.nicolae.lupascu@tutanota.com>
2021-10-25 13:14:58 +0300

    Simulate 125k and 125e5 output signal
    - Fixed negative reset
    - Commented code
    - Cleaned code
    - Simulations with 125kHz and 125e5Hz wave

--- project_4/sources/vhdl/clock_divider.vhd
+++ project_4/sources/vhdl/clock_divider.vhd
@@ -1,39 +1,21 @@
 ----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
+-- Company: TUAS
+-- Engineers: Veronica Zanella, Cristian Nicolae Lupascu 
 -- Create Date: 18.10.2021 12:27:40
--- Design Name: 
+-- Design Name: clock_divider
 -- Module Name: clock_divider - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
+-- Project Name: LAB4
+-- Target Devices: PYNQ-Z2
+-- Description: Clock divider
 ----------------------------------------------------------------------------------
-
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
 
 entity clock_divider is
-    generic(output_f: integer:= 125e5 --Hz
+    generic(output_f: integer:= 125e3 --Hz
     );
     Port (  sysclk : in std_logic;
             n_Reset: in std_logic;
@@ -42,27 +24,30 @@ entity clock_divider is
 end clock_divider;
 
 architecture Behavioral of clock_divider is
-    signal count: integer := 0;
-    signal max: integer := (125e6/output_f)/2 -1;
+    
+    constant SYSCLK_FREQUENCY: natural := 125e6; -- Hz
+    
+    signal count: natural := 0;
+    signal half_period_max: natural := (SYSCLK_FREQUENCY/output_f)/2 -1;
     signal output_copy: std_logic := '0';
     
 begin
+
     process(sysclk, n_Reset) is
     begin
-        if n_Reset='1' then
+        if n_Reset='0' then -- reset the state
             count <= 0;
             output_copy <= '0';
-            
-        elsif sysclk'event and sysclk = '1' then
-            if count >= max then
+        elsif rising_edge(sysclk) then
+            if count >= half_period_max then -- invert the output signal at half period
                 count <= 0;
                 output_copy <= not output_copy;
             else
                 count <= count + 1;
-                output_copy <= output_copy;
-            end if;
-            
-        end if;         
+            end if; -- counter login
+        end if; -- signal conditions
     end process;
+    
     output_s <= output_copy;
+    
 end Behavioral;
